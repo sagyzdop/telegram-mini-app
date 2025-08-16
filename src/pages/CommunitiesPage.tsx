f7a7cd4c340b8134d6577add371263596d9207ac
sagyzdop <sagyzdop+github@gmail.com>
2025-08-17 02:48:51 +0500

    pages

--- src/pages/CommunitiesPage.tsx
+++ src/pages/CommunitiesPage.tsx
@@ -19,49 +19,42 @@ export const CommunitiesPage: FC = () => {
   return (
     <Page back={false}>
       <Input placeholder="Search Communities" before={<Icon24SunLow />} />
-      <List>
-        {/* <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        > */}
-        <Divider />
-        <Link to="/ton-connect">
-          <Cell
-            after={<Badge type="number">4</Badge>}
-            before={<Avatar size={48} />}
-            description="We are the club that does fencing."
-            interactiveAnimation="background"
-            subtitle="Subtitle"
-          >
-            NU Fencing Club
-          </Cell>
-        </Link>
-        <Divider />
-        <Link to="/ton-connect">
-          <Cell
-            after={<Badge type="number">4</Badge>}
-            before={<Avatar size={48} />}
-            description="We are the club that does fencing."
-            interactiveAnimation="background"
-            subtitle="Subtitle"
-          >
-            NU Fencing Club
-          </Cell>
-        </Link>
-        <Divider />
-        <Link to="/ton-connect">
-          <Cell
-            after={<Badge type="number">4</Badge>}
-            before={<Avatar size={48} />}
-            description="We are the club that does fencing."
-            interactiveAnimation="background"
-            subtitle="Subtitle"
-          >
-            NU Fencing Club
-          </Cell>
-        </Link>
-        <Divider />
-      </List>
+      <Link to="/ton-connect">
+        <Cell
+          after={<Badge type="number">4</Badge>}
+          before={<Avatar size={48} />}
+          description="We are the club that does fencing."
+          interactiveAnimation="background"
+          subtitle="Subtitle"
+        >
+          NU Fencing Club
+        </Cell>
+      </Link>
+      <Divider />
+      <Link to="/ton-connect">
+        <Cell
+          after={<Badge type="number">4</Badge>}
+          before={<Avatar size={48} />}
+          description="We are the club that does fencing."
+          interactiveAnimation="background"
+          subtitle="Subtitle"
+        >
+          NU Fencing Club
+        </Cell>
+      </Link>
+      <Divider />
+      <Link to="/ton-connect">
+        <Cell
+          after={<Badge type="number">4</Badge>}
+          before={<Avatar size={48} />}
+          description="We are the club that does fencing."
+          interactiveAnimation="background"
+          subtitle="Subtitle"
+        >
+          NU Fencing Club
+        </Cell>
+      </Link>
+      <Divider />
     </Page>
   );
 };
